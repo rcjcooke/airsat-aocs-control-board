4f8e1621a2680b670dfcd2a557a086d6739ce944
Ray Cooke <rcjcooke@gmail.com>
2026-07-31 18:22:03 +0100

    Upated logging

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -15,16 +15,19 @@ void setup() {
   currentTelemetry.momentum = 0.0f;
   currentTelemetry.propellant = 1000; 
 
+  Serial.println("[main] Initialising OBC Link...");
   obcLink.begin();
+  Serial.println("[main] Initialising Reaction Wheel Controller...");
   wheelController.begin();
   
-  Serial.println("Flight Core Activated: Pure Command Router with Angular Momentum Telemetry.");
+  Serial.println("[main] AOCS Control Startup Complete.");
 }
 
 void loop() {
   wheelController.update();
 
   if (obcLink.isCommandAvailable()) {
+    Serial.printf("[main] New command received from OBC: Torque=% .3f Nm, Thrusts= % .3f, % .3f, % .3f, % .3f N.", workingCommand.torque, workingCommand.thrust[0] , workingCommand.thrust[1], workingCommand.thrust[2], workingCommand.thrust[3]);
     workingCommand = obcLink.getLatestCommand();
     wheelController.setTargetTorque(workingCommand.torque);
   }
@@ -39,11 +42,11 @@ void loop() {
   }
 
   static uint32_t printTimer = 0;
-  if (millis() - printTimer >= 1000) {
+  if (millis() - printTimer >= 500) {
     printTimer = millis();
     
     // Diagnostics display comparing raw rotational velocity (Hz) alongside calculated momentum
-    Serial.printf("[AOCS Control Log] CMD Torque: %.3f Nm | Wheel Speed: %.2f RPM | Stored Momentum: %.5f kg*m^2/s\n",
+    Serial.printf("[main] Target Torque: %.3f Nm | Wheel Speed: %.2f RPM | Stored Momentum: %.5f kg*m^2/s\r\n",
                   workingCommand.torque,
                   wheelController.getAngularVelocity() * 60.0f,
                   wheelController.getAngularMomentum());
